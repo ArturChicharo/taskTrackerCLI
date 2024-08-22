22e33415d68f0d3ae973b6eaf91ce320ec68810e
ArturChicharo <87455562+ArturChicharo@users.noreply.github.com>
2024-08-22 18:47:56 +0100

    Added "Delete" argument
    Added delete argument as "-d"/"--d".
    Takes accepts task id as integer.

--- tasktrackercli.py
+++ tasktrackercli.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-a", "--add", help="Type task description", required=False)
 parser.add_argument("-u", "--update", help="Specify task id and updated description with the format 'id:description'", required=False)
+parser.add_argument("-d", "--delete", help="Delete task by id", required=False, type=int)
 
 args = parser.parse_args()
 
@@ -17,42 +18,45 @@ current_time = datetime.now()
 formatted_time = current_time.strftime('%H:%M %d %B %Y')
 
 def addTask():
-    tasks = args.add
+    try:
+        tasks = args.add
 
-    if not os.path.exists(file_path):
-        tasks = [
-            {
-                "id": 1,
-                "description": args.add,
-                "status": "todo",
-                "createdAt": formatted_time,
-                "updatedAt": formatted_time
-            }
-        ]
+        if not os.path.exists(file_path):
+            tasks = [
+                {
+                    "id": 1,
+                    "description": args.add,
+                    "status": "todo",
+                    "createdAt": formatted_time,
+                    "updatedAt": formatted_time
+                }
+            ]
 
-        with open(file_path, 'w') as file:
-            json.dump(tasks, file, indent=4)
-    else:
-        with open(file_path, 'r') as file:
-            try:
-                data = json.load(file)
-            except json.JSONDecodeError:
-                data = []
+            with open(file_path, 'w') as file:
+                json.dump(tasks, file, indent=4)
+        else:
+            with open(file_path, 'r') as file:
+                try:
+                    data = json.load(file)
+                except json.JSONDecodeError:
+                    data = []
 
-            last_id = max(item['id'] for item in data) if data else 0
+                last_id = max(item['id'] for item in data) if data else 0
 
-            new_task = {
-                'id': last_id + 1,
-                'description': args.add,
-                'status': 'todo',
-                "createdAt": formatted_time,
-                "updatedAt": formatted_time
-            }
+                new_task = {
+                    'id': last_id + 1,
+                    'description': args.add,
+                    'status': 'todo',
+                    "createdAt": formatted_time,
+                    "updatedAt": formatted_time
+                }
 
-            data.append(new_task)
-        
-        with open(file_path, 'w') as file:
-            json.dump(data, file, indent=4)
+                data.append(new_task)
+            
+            with open(file_path, 'w') as file:
+                json.dump(data, file, indent=4)
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 def updateTask():
     try:
@@ -74,8 +78,34 @@ def updateTask():
     except Exception as e:
         print(f"An error occurred: {e}")
 
+def deleteTask():
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        
+        task_deleted = False
+
+        for task in data:
+            if task["id"] == args.delete:
+                data.remove(task)
+                task_deleted = True
+        
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+
+            if task_deleted:
+                print("Task successfully deleted.")
+            else:
+                print("No task exists with specified id.")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 if args.add:
     addTask()
 
 if args.update:
     updateTask()
+
+if args.delete:
+    deleteTask()
